550f4fadb8f7defacdc210ff33f456ef6ae588a6
coinenjoyerplayah12 <86243061+coinenjoyerplayah12@users.noreply.github.com>
2021-06-21 21:38:47 +0700

    Update ERC20.sol

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -246,14 +246,13 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
      * - `account` cannot be the zero address.
      */
     function _mint(address account, uint256 amount) internal virtual {
-        // require(account != address(0), "ERC20: mint to the zero address");
+        require(account != address(0), "ERC20: mint to the zero address");
 
-        // _beforeTokenTransfer(address(0), account, amount);
+        _beforeTokenTransfer(address(0), account, amount);
 
-        // _totalSupply += amount;
-        // _balances[account] += amount;
-        // emit Transfer(address(0), account, amount);
-        return false;
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
     }
 
     /**
